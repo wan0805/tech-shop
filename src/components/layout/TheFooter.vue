2de78ab37e8f92731b5ef95186ccbf6acedbff86
Wanderson Duarte <wanderso_ferreira_17@hotmail.com>
2026-06-02 05:43:43 -0300

    feat: create footer layout

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <footer
+    class="w-full h-40 flex items-center flex-col justify-center gap-3 bg-[#1a1a1a] pt-12 py-4 text-center text-white"
+  >
+    <router-link class="text-white font-secondary text-2xl font-bold tracking-widest" to="/">
+      Tech Shop
+    </router-link>
+    <p>Alguns direitos reservados.</p>
+  </footer>
+</template>
+
+<style scoped></style>
