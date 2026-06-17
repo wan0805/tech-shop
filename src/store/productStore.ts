074b69fa90c3e5db6caedcb19f009d356a5a287a
Wanderson Duarte <wanderso_ferreira_17@hotmail.com>
2026-06-17 04:27:49 -0300

    refactor: implement filter and sort logic in filteredProducts computed

--- src/store/productStore.ts
+++ src/store/productStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 import { useFetch } from '@/composables/useFetch'
 
@@ -8,17 +8,40 @@ const allowedCategories = ['smartphones', 'laptops', 'tablets']
 export const useProductsStore = defineStore('products', () => {
   const { data: products, product, isLoading, error, getProducts, getProductDetail } = useFetch()
 
+  const filter = ref<string>('')
+  const sort = ref<string>('')
+  const filterOptions = ['laptops', 'smartphones']
+  const sortOptions = ['Price, high to low', 'Price, low to High']
+
   const filteredProducts = computed(() => {
-    return products.value.filter((product) => {
+    //  apenas categorias permitidas
+    let filtered = products.value.filter((product) => {
       return allowedCategories.includes(product.category)
     })
+
+    if (filter.value && filter.value.trim() !== '') {
+      const searchLower = filter.value.toLowerCase()
+
+      filtered = filtered.filter((item) => {
+        return item.category.toLowerCase().includes(searchLower)
+      })
+    }
+
+    if (sort.value === 'Price, high to low') {
+      filtered = filtered.sort((a, b) => b.price - a.price)
+    }
+    if (sort.value === 'Price, low to High') {
+      filtered = filtered.sort((a, b) => a.price - b.price)
+    }
+
+    return filtered
   })
 
   function onLoadProducts() {
     getProducts('products?limit=900')
   }
 
-  async function onLoadProductDetail(id: any) {
+  async function onLoadProductDetail(id: number) {
     getProductDetail(id)
   }
 
@@ -27,6 +50,10 @@ export const useProductsStore = defineStore('products', () => {
     productDetail: product,
     isLoading,
     error,
+    filter,
+    sort,
+    filterOptions,
+    sortOptions,
     onLoadProducts,
     onLoadProductDetail,
   }
