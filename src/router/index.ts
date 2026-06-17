01c400b3c3ce4c4a91adf7810976043037fbeeac
Wanderson Duarte <wanderso_ferreira_17@hotmail.com>
2026-06-17 05:36:27 -0300

    feat: implement dynamic browser page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,19 +7,29 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      meta: { title: 'Home' },
       component: HomeView,
     },
     {
       path: '/product/:id',
       name: 'productDetail',
+      meta: { title: 'Loading Product...' },
       component: () => import('../views/ProductView.vue'),
     },
     {
       path: '/cart',
       name: 'cart',
+      meta: { title: 'Shopping Cart' },
       component: () => import('../views/CartView.vue'),
     },
   ],
+})
+
+router.beforeEach((to) => {
+  const baseTitle = 'Tech Shop'
+  const pageTitle = to.meta.title as string
+
+  document.title = pageTitle ? `${pageTitle} | ${baseTitle}` : baseTitle
 })
 
 export default router
